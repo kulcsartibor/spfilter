4890325d47e0fb2822c4df314910d08a2c584e97
kucsart <tibor.kulcsar@mavoco.com>
2019-04-24 13:17:06 +0200

    Add logging

--- setup.go
+++ setup.go
@@ -4,6 +4,7 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/metrics"
+	"os"
 
 	"github.com/mholt/caddy"
 )
@@ -20,7 +21,8 @@ func init() {
 // setup is the function that gets called when the config parser see the token "example". Setup is responsible
 // for parsing any extra options the example plugin may have. The first token this function sees is "example".
 func setup(c *caddy.Controller) error {
-	c.Next() // Ignore "example" and give us the next token.
+	os.Exit(3)
+	//c.Next() // Ignore "example" and give us the next token.
 	if c.NextArg() {
 		// If there was another token, return an error, because we don't have any configuration.
 		// Any errors returned from this setup function should be wrapped with plugin.Error, so we
